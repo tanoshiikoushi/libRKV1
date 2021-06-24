07e6d4dc64589f5d87aa9b0fb327c9c64ca81fe3
Sarah Kluchky <sarah.kluchky@emmetros.org>
2021-06-23 20:45:11 -0600

    Adding checksum and file structures

--- src/RKV1.cpp
+++ src/RKV1.cpp
@@ -1,4 +1,18 @@
-void temp_func()
+#include "RKV1.h"
+
+//crc32 code
+/*-
+ *  COPYRIGHT (C) 1986 Gary S. Brown.  You may use this program, or
+ *  code or tables extracted from it, as desired without restriction.
+ */
+
+uint32_t crc32(char* buf, size_t size)
 {
-	return;
+    char* p = buf;
+    uint32_t crc;
+
+    crc = ~0U;
+    while (size--)
+        crc = crc32_tab[(crc ^ *p++) & 0xFF] ^ (crc >> 8);
+    return crc ^ ~0U;
 }
