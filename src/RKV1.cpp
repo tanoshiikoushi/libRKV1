0d26579092691da766fb0fa408e39fe63fad79a3
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-08-13 21:28:00 -0600

    I'm special lol

--- src/RKV1.cpp
+++ src/RKV1.cpp
@@ -14,7 +14,7 @@ bool RKV1File::load(const u8* buf_to_copy, const u64 buf_size) {
     // set our current location to the variable which holds the directory count
     u64 current_location = this->data_size - 0x4;
     this->directory_count = read_32LE(&this->data[current_location]);
-    this->file_count = read_32LE(&this->data[current_location]);
+    this->file_count = read_32LE(&this->data[current_location - 0x4]);
 
     if (this->directory_count == 0 || this->file_count == 0) {
         printf("Invalid directory or file counts: %d directories detected, %d files detected\n", this->directory_count, this->file_count);
